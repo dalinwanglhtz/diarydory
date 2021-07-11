194a34bf99814b78ba1d129ff3513f0bd05f9168
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-07-11 21:51:23 +1000

    Add diary entry canvas and controller

---
+++ force-app/main/default/lwc/diaryEntryCanvas/diaryEntryCanvas.js
@@ -0,0 +1,32 @@
+import { LightningElement, track } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import saveDiaryEntry from '@salesforce/apex/DiaryEntryController.saveDiaryEntry';
+
+export default class DiaryEntryCanvas extends LightningElement {
+
+    @track errorMessage;
+
+    handleClick() {
+        let value = this.template.querySelector('lightning-input-rich-text').value.replace(/<([^>]+)>/ig, '');
+
+        saveDiaryEntry({entryData:value})
+        .then(result => {
+            const toastEvent = new ShowToastEvent({
+                title: 'Success!',
+                message: 'Record saved successfully!',
+                variant: 'success'
+            });
+            this.dispatchEvent(toastEvent);
+            console.log('Result: ', result);
+        })
+        .catch(error => {
+            this.errorMessage = error.body.message;
+            console.log('Error: ', this.errorMessage);
+        });
+    }
+
+    handleClear() {
+        this.template.querySelector('form').reset();
+        this.template.querySelector('lightning-input-rich-text').value = null;
+    }
+}
